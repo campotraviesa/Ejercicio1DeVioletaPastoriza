b7a8bbafaf2e1afed74e53925d17d5bd259e5ccc
Violeta <Violeta@NEGRI>
2022-07-05 18:46:46 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio1DeVioletaPastoriza/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio1DeVioletaPastoriza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pesos;
+            double dolares;
+            double euros;
+            Console.Title = "Convertir pesos a dólares y euros";
+            Console.Write("Ingrese el monto en pesos:");
+            pesos = double.Parse(Console.ReadLine());
+            dolares = pesos/230;
+            Console.Write($"Con ese monto se pueden comprar {dolares} dolares.");
+            euros = (dolares/1.17);
+            Console.Write($" Eso equivale a {euros} euros");
+        }
+    }
+}
